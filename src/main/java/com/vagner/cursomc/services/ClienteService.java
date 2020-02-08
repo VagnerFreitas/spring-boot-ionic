3965f4645fae2c31e6989bf2ab2205241ed8d890
Vagner Freitas <vagner_freitas@hotmail.com>
2020-02-08 17:17:41 -0300

    Endpoint clientes/id disponivel

---
+++ src/main/java/com/vagner/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.vagner.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vagner.cursomc.domain.Cliente;
+import com.vagner.cursomc.repositories.ClienteRepository;
+import com.vagner.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente cliente = repo.findById(id).orElse(null);
+		if (cliente == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado id: " + id
+					+ " Tipo: " + Cliente.class.getName());
+		}
+		return repo.findById(id).orElse(null);
+	}
+}
