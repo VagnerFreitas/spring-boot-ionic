0a643d17216d562b39d41bea90f0e9b971e3e4e2
Vagner Freitas <vagner_freitas@hotmail.com>
2020-02-09 23:33:19 -0300

    ItemPedido

--- src/main/java/com/vagner/cursomc/domain/Pedido.java
+++ src/main/java/com/vagner/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.vagner.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -31,6 +33,8 @@ public class Pedido implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco endereco;
+	
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {}
 
@@ -81,6 +85,14 @@ public class Pedido implements Serializable{
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
@@ -106,6 +118,5 @@ public class Pedido implements Serializable{
 			return false;
 		return true;
 	}
-
 	
 }
--- src/main/java/com/vagner/cursomc/domain/Produto.java
+++ src/main/java/com/vagner/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.vagner.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -33,6 +35,8 @@ public class Produto implements Serializable {
 	)
 	private List<Categoria> categorias = new ArrayList<Categoria>();
 	
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {}
 
 	public Produto(Integer id, String nome, Double preco) {
@@ -41,8 +45,16 @@ public class Produto implements Serializable {
 		this.nome = nome;
 		this.preco = preco;
 	}
+	
 
-
+	public List<Pedido> getPedidos(){
+		List<Pedido> pedidos = new ArrayList<>();
+		for (ItemPedido item : itens) {
+			pedidos.add(item.getPedido());
+		}
+		
+		return pedidos;
+	}
 
 	public Integer getId() {
 		return id;
@@ -75,7 +87,38 @@ public class Produto implements Serializable {
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
-	
-	
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
